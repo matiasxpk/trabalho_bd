db8c7d07bff32923539c8b163271562252bc9d6a
Júlia Freitas <juliafreitas@alu.ufc.br>
2023-07-08 16:19:04 -0300

    formatando tamanho dos botões

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import tkinter as tk
 # Estabelecer a conexão com o banco de dados
 conn = psycopg2.connect(
     host="localhost",
-    database="DoacaoDeSangue",
+    database="doacaoDeSangue",
     user="postgres",
-    password="jogo1234"
+    password="2612"
 )
 
 cur = conn.cursor()
@@ -18,88 +18,90 @@ class Interface:
         self.root.configure(bg="white")
         self.root.title("Hematocare")
 
+        # Configurar peso das linhas e colunas
+        for i in range(12):
+            self.root.grid_rowconfigure(i, weight=1)
+        for i in range(2):
+            self.root.grid_columnconfigure(i, weight=1)
+
         self.result_label = tk.Label(root, text="")
-        self.result_label.pack()
+        self.result_label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+       
 
         self.label1 = tk.Label(root, text="Digite o CPF:")
-        self.label1.pack()
+        self.label1.grid(row=1, column=0, sticky="e")
         self.entry1 = tk.Entry(root)
-        self.entry1.pack()
+        self.entry1.grid(row=1, column=1, sticky="w")
 
         self.label2 = tk.Label(root, text="Digite o nome:")
-        self.label2.pack()
+        self.label2.grid(row=2, column=0, sticky="e")
         self.entry2 = tk.Entry(root)
-        self.entry2.pack()
+        self.entry2.grid(row=2, column=1, sticky="w")
 
         self.label3 = tk.Label(root, text="Digite a idade:")
-        self.label3.pack()
+        self.label3.grid(row=3, column=0, sticky="e")
         self.entry3 = tk.Entry(root)
-        self.entry3.pack()
+        self.entry3.grid(row=3, column=1, sticky="w")
 
         self.label4 = tk.Label(root, text="Digite o E-mail:")
-        self.label4.pack()
+        self.label4.grid(row=4, column=0, sticky="e")
         self.entry4 = tk.Entry(root)
-        self.entry4.pack()
+        self.entry4.grid(row=4, column=1, sticky="w")
+
 
         self.label5 = tk.Label(root, text="Digite o logradouro:")
-        self.label5.pack()
+        self.label5.grid(row=5, column=0, sticky="e")
         self.entry5 = tk.Entry(root)
-        self.entry5.pack()
+        self.entry5.grid(row=5, column=1, sticky="w")
 
         self.label6 = tk.Label(root, text="Digite o número:")
-        self.label6.pack()
+        self.label6.grid(row=6, column=0, sticky="e")
         self.entry6 = tk.Entry(root)
-        self.entry6.pack()
+        self.entry6.grid(row=6, column=1, sticky="w")
 
         self.label7 = tk.Label(root, text="Digite o bairro:")
-        self.label7.pack()
+        self.label7.grid(row=7, column=0, sticky="e")
         self.entry7 = tk.Entry(root)
-        self.entry7.pack()
+        self.entry7.grid(row=7, column=1, sticky="w")
 
         self.label8 = tk.Label(root, text="Digite a cidade:")
-        self.label8.pack()
+        self.label8.grid(row=8, column=0, sticky="e")
         self.entry8 = tk.Entry(root)
-        self.entry8.pack()
+        self.entry8.grid(row=8, column=1, sticky="w")
 
         self.label9 = tk.Label(root, text="Digite o CEP:")
-        self.label9.pack()
+        self.label9.grid(row=9, column=0, sticky="e")
         self.entry9 = tk.Entry(root)
-        self.entry9.pack()
+        self.entry9.grid(row=9, column=1, sticky="w")
 
         self.label10 = tk.Label(root, text="Digite o tipo sanguineo:")
-        self.label10.pack()
+        self.label10.grid(row=10, column=0, sticky="e")
         self.entry10 = tk.Entry(root)
-        self.entry10.pack()
+        self.entry10.grid(row=10, column=1, sticky="w")
 
-        self.label11 = tk.Label(root, text="Digite a senha:")
-        self.label11.pack()
-        self.entry11 = tk.Entry(root, show="*")
-        self.entry11.pack()
-
-        self.label12 = tk.Label(root, text="Digite o CPF do doador:")
-        self.label12.pack()
-        self.entry12 = tk.Entry(root)
-        self.entry12.pack()
-
-        self.label13 = tk.Label(root, text="Digite o telefone:")
-        self.label13.pack()
-        self.entry13 = tk.Entry(root)
-        self.entry13.pack()
+        self.label11 = tk.Label(root, text="Digite o telefone:")
+        self.label11.grid(row=11, column=0, sticky="e")
+        self.entry11 = tk.Entry(root)
+        self.entry11.grid(row=11, column=1, sticky="w")
 
         self.button1 = tk.Button(root, text="Registrar Novo Doador", command=self.register_doador)
-        self.button1.pack()
+        self.button1.grid(row=12, column=0, columnspan=2)
+
+
 
         """self.button2 = tk.Button(root, text="Registrar Hospital", command=self.register_hospital)
-        self.button2.pack() 
+        self.button2.grid(row=12, column=0, columnspan=2)
 
         self.button3 = tk.Button(root, text="Registrar Funcionário", command=self.register_funcionario)
-        self.button3.pack()"""
+        self.button3.grid(row=13, column=0, columnspan=2)"""
 
         self.button4 = tk.Button(root, text="Remover Doador", command=self.remove_doador)
-        self.button4.pack()
+        self.button4.grid(row=13, column=0, columnspan=2)
 
         self.button5 = tk.Button(root, text="Consultar Doador", command=self.consultar_doador)
-        self.button5.pack()
+        self.button5.grid(row=14, column=0, columnspan=2)
+
 
     def register_doador(self):
         cpf = self.entry1.get()
@@ -112,7 +114,6 @@ class Interface:
         cidade = self.entry8.get()
         cep = self.entry9.get()
         tipo_sanguineo = self.entry10.get()
-        senha = self.entry11.get()
         telefone = self.entry13.get()
 
         # Executar instrução SQL para inserir os dados no banco de dados
@@ -132,7 +133,7 @@ class Interface:
         print("Cidade:", cidade)
         print("CEP:", cep)
         print("Tipo Sanguíneo:", tipo_sanguineo)
-        print("Senha:", senha)
+
 
     def remove_doador(self):
         cpf = self.entry12.get()
